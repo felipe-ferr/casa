3115736789acc99e20ac38ef4b607997aa285947
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-11-21 23:51:30 -0300

    limitar entre 16 e 25 graus se for digitado valores maiores

--- temperaturaQuarto.js
+++ temperaturaQuarto.js
@@ -2,4 +2,12 @@ function temperaturaQuarto(){
     let temp = document.getElementById('temp-ac-quarto').value;
 
     document.getElementById("temperatura-quarto").innerHTML = temp;
+
+    if(temp > 28){
+        document.getElementById("temperatura-quarto").innerHTML = 28;
+    }
+
+    if(temp < 16){
+        document.getElementById("temperatura-quarto").innerHTML = 16;
+    }
 }
